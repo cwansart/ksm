ea87c405b9bf0261f7daec1226d8fb1053be3d05
Christian Wansart <cwansart@users.noreply.github.com>
2017-08-29 19:24:12 +0200

    Fixed IE issues/added babel-polyfill

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -1,6 +1,9 @@
 
 window._ = require('lodash');
 
+// fix IE Promises issue
+import 'babel-polyfill'
+
 /**
  * We'll load jQuery and the Bootstrap jQuery plugin which provides support
  * for JavaScript based Bootstrap features such as modals and tabs. This
