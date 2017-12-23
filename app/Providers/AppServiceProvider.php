8d4d04c682faea118db63b689fba70347d33123c
Christian Wansart <cwansart@users.noreply.github.com>
2017-12-23 23:51:51 +0100

    Fix for sql length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
+
         Validator::extend('image64', function ($attribute, $value, $parameters, $validator) {
             $type = explode('/', explode(':', substr($value, 0, strpos($value, ';')))[1])[1];
             if (in_array($type, $parameters)) {
